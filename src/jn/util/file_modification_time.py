fab40267e1bcfe2b413734e3e1c88338896d8578
yelircaasi <isaac.r.riley@gmail.com>
2024-06-19 16:41:08 +0200

    chore: separate fast and readable file modification time functions

--- src/jn/util/file_modification_time.py
+++ src/jn/util/file_modification_time.py
@@ -3,11 +3,19 @@ from pathlib import Path
 import time
 
 
-def time_created(path: Path | str) -> str:
-    time_created = time.strptime(time.ctime(os.path.getctime(path)))
+def time_created_(path: Path | str) -> str:
+    return os.path.getctime(path)
+    
+
+def time_created_readable(path: Path | str) -> str:
+    time_created = time.strptime(time.ctime(time_created(path)))
     return time.strftime("%Y-%m-%d %H:%M:%S", time_created)
 
 
 def time_modified(path: Path | str) -> str:
-    time_modified = time.strptime(time.ctime(os.path.getmtime(path)))
+    return os.path.getmtime(path)
+
+
+def time_modified_readable(path: Path | str) -> str:
+    time_modified = time.strptime(time.ctime(time_modified(path)))
     return time.strftime("%Y-%m-%d %H:%M:%S", time_modified)
